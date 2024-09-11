3c12ea927f8dc589f4a32319cd18059e8aa2af18
Rohit Besra <rohitkumarbesra786@gmail.com>
2024-09-11 21:41:11 +0530

    footer_1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <div className='flex-grow'>
+        <div className="flex-grow">
           <main>
             <Header/>
           </main>
