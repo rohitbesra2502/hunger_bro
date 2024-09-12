d337cb33a472c59ef9726633467e73799ee50f9b
Ashwani kumar kushwaha <ashwaniku6266@gmail.com>
2024-09-12 19:33:39 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import Header from './component/Header' 
+import Header from './component/Header'
+import Footer from './component/Footer'
 
 function App() {
   return (
     <>
-      <Header/>
-    
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-grow">
+          <main>
+            <Header />
+          </main>
+        </div>
+        <Footer />
+      </div>
+     
     </>
   )
 }
 export default App;
+
+
