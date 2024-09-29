7670bcd85f91001be3ed52a83bbcc153b850b57e
Ashwani kumar kushwaha <ashwaniku6266@gmail.com>
2024-09-30 00:06:41 +0530

    ................

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         setIsMenuOpen(!isMenuOpen);
         alert("clicked !!");
     };
- 
+
     return (
         <>
             <header className="w-full py-9">
@@ -34,25 +34,25 @@ function Header() {
                         {/* <div className={`${isMenuOpen ? "capitalize font-medium items-center w-full lg:w-auto absolute lg:relative top-14 lg:top-0" : ""} `} id="menu"> */}
                         {/* <ul className={` ${isMenuOpen ? "items-center justify-between gap-x-8 full flex flex-col lg:flex lg:flex-row lg:max-lg:flex-col bg-yellow-500 lg:bg-white" : "md:hidden"}`}> */}
 
-                            <div className="capitalize font-medium items-center w-full lg:w-auto absolute lg:relative top-14 lg:top-0" id="menu">
-                                <ul className="items-center justify-between gap-x-8 full flex flex-col lg:flex lg:flex-row lg:max-lg:flex-col bg-yellow-500 lg:bg-white">
-                                    <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
-                                        <a href="/">Home</a>
-                                    </li>
-                                    <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
-                                        <a href="/">About Us</a>
-                                    </li>
-                                    <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
-                                        <a href="/">Our Special</a>
-                                    </li>
-                                    <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
-                                        <a href="/">Services</a>
-                                    </li>
-                                    <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
-                                        <a href="/">Contact</a>
-                                    </li>
-                                </ul>
-                            </div>
+                        <div className="capitalize font-medium items-center w-full lg:w-auto absolute lg:relative top-14 lg:top-0" id="menu">
+                            <ul className="items-center justify-between gap-x-8 full flex flex-col lg:flex lg:flex-row lg:max-lg:flex-col">
+                                <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
+                                    <a href="/">Home</a>
+                                </li>
+                                <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
+                                    <a href="/">About Us</a>
+                                </li>
+                                <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
+                                    <a href="/">Our Special</a>
+                                </li>
+                                <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
+                                    <a href="/">Services</a>
+                                </li>
+                                <li className="py-3 lg:py-0 border-b border-slate-100 w-full text-center lg:w-auto lg:border-0">
+                                    <a href="/">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
 
 
 
--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,8 +1,20 @@
+import Product1 from '../assets/product1.webp';
+import Product2 from '../assets/product5.jpeg';
+import Product3 from '../assets/product3.webp';
+import Product4 from '../assets/product4.png';
+import Button from './Button'
+
+const Products = [
+    { id: 1, name: 'Tomato Garlic Pizza ', price: 159, rating: 4.5, img: Product1 },
+    { id: 2, name: 'Overloaded Chow Mein', price: 255, rating: 4.5, img: Product2 },
+    { id: 3, name: 'Burger With French Fries', price: 154, rating: 4.5, img: Product3 },
+    { id: 4, name: 'Burger2', price: 200, rating: 4.8, img: Product4 }
+];
 
 const Product = () => {
     return (
         <>
-            <section className="py-5">
+            <section className="container mx-auto py-5">
                 <div className="flex items-center justify-center flex-col">
                     <h5 className="uppercase text-red-500 font-semibold text-lg tracking-wider mb-6">Customer favorites</h5>
                     <h2 className="uppercase text-amber-500 text-5xl tracking-wider font mb-6">Popular products</h2>
@@ -10,12 +22,23 @@ const Product = () => {
                 </div>
 
                 <div className="">
-                    <ul className="px-0">
-                        <li className="">
-                            <div className=""></div>
-                        </li>
+                    <ul className="grid grid-cols-4 gap-4">
+                        {Products.map((Productss) => (
+                            <li className="" key={Productss.id}>
+                                <div className="relative">
+                                    {Productss.img ? <img src={Productss.img} alt={Productss.name} className="product_images object-cover w-full" /> : <span>no image</span>}
+                                    <h3 className='text-xl font-semibold'>{Productss.name}</h3>
+                                    <label htmlFor="rating">{Productss.rating}</label>
+                                    <div className="flex justify-between items-center">
+                                        <label className="text-md text-slate-500">Rs.{Productss.price}</label>
+                                        <Button title="Order" />
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
                     </ul>
                 </div>
+
             </section>
         </>
     )
