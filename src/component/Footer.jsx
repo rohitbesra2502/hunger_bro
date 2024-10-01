18acea68d411b46d218364eab9ff11b94e56fbf2
Rohit Besra <rohitkumarbesra786@gmail.com>
2024-10-01 16:10:41 +0530

    footer completed👍

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <div className="footer_container bg-yellow-100 ">
             {/* //ashwani se puchna hai */}
-            <div className="lg:container lg:mx-auto px-2 lg:px-3 pt-14 pb-28">   
-                <div className="grid grid-cols-3 gap-4 border border-sky-500">
-                    <div className=" p-1">
+            <div className="lg:container lg:mx-auto px-2 lg:px-3 pt-14 pb-28">
+                <div className="grid grid-cols-3 gap-4">
+                    <div className="p-4">
                         <div className="uppercase font-bold text-3xl ">
                             <span className="text-amber-400">hunger</span>
                             <span className="text-slate-950">-</span>
@@ -23,26 +23,25 @@ const Footer = () => {
                             <h5 className="font-['Poppins'] font-normal  leading-8">Office location - near by Overbridge telibandha, Raipur,  Chhattisgarh - 492001</h5>
                         </div>
                     </div>
-                    <div className=" p-1">
-                        <h6 className="font-['Poppins'] font-medium text-xl leading-8">Quick Link</h6>
-                        <div className="flex font-['Poppins'] font-normal leading-8 py-6">
+                    <div className="p-4  ">
+                        <h6 className="font-['Poppins'] pl-20 font-medium text-xl leading-8 ">Quick Link</h6>
+                        <div className="flex justify-end font-['Poppins'] text-sm leading-6  py-6">
                             <div className="">
                                 <ul>
-                                    <li className="p-1">Home</li>
-                                    <li className="p-1">About Us</li>
-                                    <li className="p-1">Explore Food</li>
-                                    <li className="p-1">Our Specials</li>
-                                    <li className="p-1">Services</li>
-                                    <li className="p-1">Contact Us</li>
+                                    <li className="">Home</li>
+                                    <li className="">About Us</li>
+                                    <li className="">Explore Food</li>
+                                    <li className="">Our Specials</li>
+                                    <li className="">Services</li>
+                                    <li className="">Contact Us</li>
                                 </ul>
                             </div>
                             <div className="pl-24">
                                 <ul>
-
-                                    <li>
-                                        <a href="/" className="p-2">
-                                            <span>
-                                                <svg width="61" height="61" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <li className="pt-3 p-4">
+                                        <a href="/" className="">
+                                            <div className="flex items-center py-1 h-0">
+                                                <svg width="41" height="41" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                     <g filter="url(#filter0_d_1_740)">
                                                         <path d="M28.5 11C19.953 11 13 17.9533 13 26.5C13 35.047 19.953 42.0006 28.5 42.0006C37.0464 42.0006 44 35.047 44 26.5C44 17.9533 37.0464 11 28.5 11ZM32.8807 20.7848C32.8807 20.8582 32.8516 20.9286 32.7997 20.9805C32.7478 21.0324 32.6774 21.0616 32.604 21.0616L30.842 21.0627C29.6794 21.0627 29.4669 21.5154 29.4669 22.4071V24.2568H32.4889C32.5683 24.2568 32.6441 24.2911 32.6964 24.3509C32.7487 24.4106 32.7731 24.4901 32.7634 24.5689L32.3331 27.8989C32.3245 27.9656 32.2918 28.0269 32.2413 28.0713C32.1908 28.1157 32.1258 28.1402 32.0586 28.1402H29.4669V36.4088C29.4669 36.4822 29.4378 36.5526 29.3859 36.6045C29.334 36.6564 29.2636 36.6856 29.1902 36.6856H25.752C25.6786 36.6856 25.6082 36.6564 25.5563 36.6045C25.5044 36.5526 25.4753 36.4822 25.4753 36.4088V28.1402H22.8753C22.8019 28.1402 22.7315 28.1111 22.6797 28.0592C22.6278 28.0073 22.5986 27.9369 22.5986 27.8635V24.5335C22.5986 24.4601 22.6278 24.3897 22.6797 24.3378C22.7315 24.2859 22.8019 24.2568 22.8753 24.2568H25.4753V22.0775C25.4753 19.1911 27.222 17.3984 30.0345 17.3984C31.2436 17.3984 32.312 17.4884 32.6403 17.5318C32.7068 17.5406 32.7678 17.5733 32.8121 17.6237C32.8563 17.6742 32.8807 17.739 32.8807 17.8061V20.7848Z" fill="#FB5B5B" />
                                                     </g>
@@ -59,18 +58,114 @@ const Footer = () => {
                                                         </filter>
                                                     </defs>
                                                 </svg>
-                                            </span>Facebook
+
+                                                <span className="mb-2">
+                                                    Facebook
+                                                </span>
+                                            </div>
                                         </a>
                                     </li>
-                                    <li className="p-2">Instagram</li>
-                                    <li className="p-2">abc@gmail.com</li>
-                                    <li className="p-2">789556423</li>
+                                    <li className="p-4">
+                                        <a href="/" className="">
+                                            <div className="flex items-center py-1 h-0">
+                                                <svg width="39" height="39" viewBox="0 0 59 59" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <g filter="url(#filter0_d_1_736)">
+                                                        <path d="M34.8361 11H20.1644C18.2649 11.002 16.4438 11.7575 15.1006 13.1006C13.7575 14.4438 13.002 16.2649 13 18.1644V32.8356C13.002 34.7351 13.7575 36.5562 15.1006 37.8994C16.4438 39.2425 18.2649 39.998 20.1644 40H34.8361C36.7355 39.998 38.5566 39.2425 39.8996 37.8993C41.2427 36.5562 41.9981 34.7351 42 32.8356V18.1644C41.9981 16.2649 41.2427 14.4438 39.8996 13.1007C38.5566 11.7575 36.7355 11.002 34.8361 11ZM39.2473 32.8356C39.2459 34.0052 38.7808 35.1266 37.9539 35.9537C37.1269 36.7808 36.0057 37.2462 34.8361 37.2477H20.1644C18.9947 37.2463 17.8733 36.781 17.0463 35.9538C16.2193 35.1267 15.7541 34.0053 15.7527 32.8356V18.1644C15.7541 16.9947 16.2193 15.8733 17.0463 15.0462C17.8733 14.219 18.9947 13.7537 20.1644 13.7523H34.8361C36.0057 13.7538 37.1269 14.2192 37.9539 15.0463C38.7808 15.8734 39.2459 16.9948 39.2473 18.1644V32.8356Z" fill="#FB5B5B" />
+                                                        <path d="M27.5002 18.5945C26.1344 18.5946 24.7993 18.9997 23.6638 19.7585C22.5282 20.5174 21.6432 21.5959 21.1206 22.8578C20.598 24.1196 20.4613 25.5081 20.7278 26.8476C20.9943 28.1871 21.652 29.4176 22.6178 30.3833C23.5836 31.349 24.8141 32.0067 26.1536 32.2731C27.4932 32.5395 28.8816 32.4027 30.1434 31.88C31.4052 31.3573 32.4837 30.4722 33.2425 29.3366C34.0013 28.201 34.4063 26.8659 34.4063 25.5001C34.4042 23.6692 33.676 21.9138 32.3813 20.6192C31.0865 19.3246 29.3311 18.5964 27.5002 18.5945ZM27.5002 29.6535C26.6788 29.6534 25.8758 29.4097 25.1929 28.9533C24.5099 28.4968 23.9776 27.8481 23.6633 27.0892C23.3491 26.3303 23.2669 25.4952 23.4272 24.6896C23.5875 23.8839 23.9831 23.1439 24.564 22.5631C25.1448 21.9823 25.8849 21.5868 26.6905 21.4265C27.4962 21.2663 28.3313 21.3486 29.0902 21.663C29.8491 21.9773 30.4977 22.5097 30.9541 23.1927C31.4104 23.8757 31.654 24.6787 31.654 25.5001C31.6529 26.6014 31.2149 27.6573 30.4362 28.4359C29.6574 29.2146 28.6015 29.6525 27.5002 29.6535Z" fill="#FB5B5B" />
+                                                        <path d="M34.6892 16.4683C34.3246 16.4682 33.9682 16.5762 33.665 16.7787C33.3618 16.9812 33.1255 17.269 32.9859 17.6059C32.8463 17.9427 32.8097 18.3133 32.8808 18.6709C32.9519 19.0285 33.1274 19.357 33.3852 19.6148C33.643 19.8727 33.9714 20.0483 34.329 20.1194C34.6866 20.1906 35.0572 20.1541 35.3941 20.0146C35.7309 19.8751 36.0188 19.6389 36.2214 19.3357C36.424 19.0326 36.5321 18.6762 36.5321 18.3116C36.5321 17.8228 36.3379 17.354 35.9924 17.0083C35.6468 16.6626 35.178 16.4684 34.6892 16.4683Z" fill="#FB5B5B" />
+                                                    </g>
+                                                    <defs>
+                                                        <filter id="filter0_d_1_736" x="0" y="0" width="59" height="59" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                                            <feOffset dx="2" dy="4" />
+                                                            <feGaussianBlur stdDeviation="7.5" />
+                                                            <feComposite in2="hardAlpha" operator="out" />
+                                                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                                                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_736" />
+                                                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_736" result="shape" />
+                                                        </filter>
+                                                    </defs>
+                                                </svg>
+
+                                                <span className="mb-1">
+                                                    Instagram
+                                                </span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li className="p-4">
+                                        <a href="/" className="">
+                                            <div className="flex items-center py-1 h-0">
+                                                <svg width="41" height="42" viewBox="0 0 61 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <g filter="url(#filter0_d_1_734)">
+                                                        <path d="M28.5 22.9963L13.6717 11.2666C13.93 11.1066 14.24 11 14.55 11H42.45C42.76 11 43.07 11.1066 43.3283 11.2666L28.5 22.9963ZM43.9483 12.1197L34.08 19.9573L43.845 31.7936C43.9483 31.5803 44 31.3671 44 31.1005V12.5995C44 12.4396 43.9483 12.2796 43.9483 12.1197ZM28.81 24.116C28.7067 24.1693 28.6033 24.2226 28.5 24.2226C28.3967 24.2226 28.2933 24.1693 28.19 24.116L23.7467 20.5971L13.93 32.5401C14.1367 32.6467 14.3433 32.7 14.55 32.7H42.45C42.6567 32.7 42.915 32.6467 43.07 32.5401L33.2533 20.5971L28.81 24.116ZM13.0517 12.1197C13 12.2796 13 12.3862 13 12.5462V31.0472C13 31.3138 13.0517 31.527 13.155 31.7403L22.92 19.9039L13.0517 12.1197Z" fill="#FB5B5B" />
+                                                    </g>
+                                                    <defs>
+                                                        <filter id="filter0_d_1_734" x="0" y="0" width="61" height="51.7" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                                            <feOffset dx="2" dy="4" />
+                                                            <feGaussianBlur stdDeviation="7.5" />
+                                                            <feComposite in2="hardAlpha" operator="out" />
+                                                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                                                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_734" />
+                                                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_734" result="shape" />
+                                                        </filter>
+                                                    </defs>
+                                                </svg>
+
+                                                <span className="mb-1">
+                                                    abc@gmail.com
+                                                </span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li className="p-4">
+                                        <a href="/" className="">
+                                            <div className="flex items-center py-1 h-0">
+                                                <svg width="41" height="41" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <g filter="url(#filter0_d_1_735)">
+                                                        <path d="M28.5 11C19.9378 11 13 17.9378 13 26.5C13 35.0622 19.9378 42 28.5 42C37.0622 42 44 35.0622 44 26.5C44 17.9378 37.0622 11 28.5 11ZM24.1352 17.2L27.3468 20.7836L25.8526 22.1166L22.641 18.533L24.1352 17.2ZM33.9374 34.9878C32.9144 35.8992 31.4202 36.0666 30.2298 35.3846C28.3202 34.2934 25.9332 32.5946 23.7322 30.0092C21.897 27.8516 20.7066 25.6816 19.9378 23.896C19.3922 22.6374 19.7022 21.1618 20.7314 20.2442L21.99 19.122L25.2326 22.7304C24.8048 23.2574 24.687 23.989 24.9474 24.6338C25.245 25.3778 25.6852 26.2334 26.33 27.1076C26.7702 27.709 27.9606 29.1846 29.9942 30.2882C29.9942 30.2944 30.0004 30.2944 30.0066 30.3006C30.6204 30.6354 31.3582 30.592 31.9348 30.2324L35.196 33.8656L33.9374 34.9878ZM35.8346 33.2766L32.5982 29.662L34.0924 28.329L37.3288 31.9436L35.8346 33.2766Z" fill="#FB5B5B" />
+                                                    </g>
+                                                    <defs>
+                                                        <filter id="filter0_d_1_735" x="0" y="0" width="61" height="61" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                                            <feOffset dx="2" dy="4" />
+                                                            <feGaussianBlur stdDeviation="7.5" />
+                                                            <feComposite in2="hardAlpha" operator="out" />
+                                                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                                                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_735" />
+                                                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_735" result="shape" />
+                                                        </filter>
+                                                    </defs>
+                                                </svg>
+
+                                                <span className="mb-1">
+                                                    9926752353
+                                                </span>
+                                            </div>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    <div className=" p-1">
-                        <h5 className="font-['Poppins'] font-medium leading-8 ">Subscribe to Our Newsletter</h5>
+                    <div className="p-4 text-right">
+                        <h5 className="font-['Poppins'] text-xl font-medium leading-8 pr-8">Subscribe to Our Newsletter</h5>
+                        <div className="flex justify-end ">
+                            {/* search box  */}
+                            <input type="search" placeholder="    Email Address" className="rounded-md border-2 border-rose-400 w-64 h-12" />
+                            {/* arrrow sign */}
+                            <div className="flex items-center relative">
+                                <input type="search" className="rounded-md border-2 border-rose-400 w-14 h-12 ml-5 "/>
+                                <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute  right-4">
+                                    <path d="M24.9982 11.9478C24.9868 11.7608 24.9114 11.583 24.7842 11.4428L14.512 0.298658C14.4382 0.212833 14.3476 0.142359 14.2454 0.0914173C14.1432 0.0404758 14.0316 0.0101108 13.9171 0.00212665C13.8027 -0.0058575 13.6878 0.00869939 13.5792 0.0449352C13.4706 0.081171 13.3706 0.138346 13.2851 0.213062C13.1997 0.287779 13.1304 0.37851 13.0815 0.479868C13.0327 0.581226 13.0052 0.691143 13.0007 0.803083C12.9962 0.915024 13.0148 1.02671 13.0553 1.13149C13.0959 1.23628 13.1576 1.33204 13.2369 1.41307L22.9919 12L13.2369 22.5869C13.1576 22.668 13.0959 22.7637 13.0553 22.8685C13.0148 22.9733 12.9962 23.085 13.0007 23.1969C13.0052 23.3089 13.0327 23.4188 13.0815 23.5201C13.1304 23.6215 13.1997 23.7122 13.2851 23.7869C13.3706 23.8617 13.4706 23.9188 13.5792 23.9551C13.6878 23.9913 13.8027 24.0059 13.9171 23.9979C14.0316 23.9899 14.1432 23.9595 14.2454 23.9086C14.3476 23.8576 14.4382 23.7872 14.512 23.7013L24.7842 12.5572C24.8594 12.4745 24.917 12.378 24.9537 12.2734C24.9904 12.1688 25.0056 12.0581 24.9982 11.9478Z" fill="#FB5B5B" />
+                                    <line x1="1" y1="12" x2="22" y2="12" stroke="#FB5B5B" stroke-width="2" stroke-linecap="round" />
+                                </svg>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
